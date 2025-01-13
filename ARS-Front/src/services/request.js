cd82df579e2cc5a29e0b63cb2caab22ed591aefc
terence <terenceshawn18@outlook.com>
2025-01-13 11:13:34 +0300

    Feat:Added request services

--- ARS-Front/src/services/request.js
+++ ARS-Front/src/services/request.js
@@ -1,3 +1,61 @@
 import axios from "axios";
 import { API_BASE_URL } from "./user";
 import { axiosInstance } from "./user";
+
+
+export const requestsService={
+        getRequests: async () => {
+            try {
+                const response= await axiosInstance.get('requests/');
+                return response.data;
+            }
+            catch (error) {
+                if(axios.isAxiosError(error)){
+                    const message = error.response?.data?.message || error.message;
+                    throw new Error(message)
+                }
+                throw new Error('An unexpected error occurred while fetching requests')
+            }
+        },
+        employeeRequests: async () => {
+            try {
+                const response= await axiosInstance.get('requests/user/');
+                return response.data;
+            }
+            catch (error) {
+                if(axios.isAxiosError(error)){
+                    const message = error.response?.data?.message || error.message;
+                    throw new Error(message)
+                }
+                throw new Error('An unexpected error occurred while fetching requests')
+            }
+        },
+        requestAction:async(id)=>{
+            try {
+                const response= await axiosInstance.post(`requests/${id}/`);
+                return response.data;
+            }
+            catch (error) {
+                if(axios.isAxiosError(error)){
+                    const message = error.response?.data?.message || error.message;
+                    throw new Error(message)
+                }
+                throw new Error('An unexpected error occurred while updating the requests')
+            }
+        },
+        deleteRequest:async(id)=>{
+            try {
+                const response= await axiosInstance.delete(`requests/${id}/delete/`);
+                return response.data;
+            }
+            catch (error) {
+                if(axios.isAxiosError(error)){
+                    const message = error.response?.data?.message || error.message;
+                    throw new Error(message)
+                }
+                throw new Error('An unexpected error occurred while deleting the requests')
+
+            }
+        }
+    
+}
