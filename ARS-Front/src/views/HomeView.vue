696d8003482c0627893a8133ed8113560cee2a76
terence <terenceshawn18@outlook.com>
2025-01-15 14:11:49 +0300

    Chore:Cleaning up initial code

--- ARS-Front/src/views/HomeView.vue
+++ ARS-Front/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main-container">
   </main>
 </template>
+<script setup>
+
+</script>
